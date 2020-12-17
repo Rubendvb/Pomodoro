aa1b7ea93293ec0e95f6cc9f9fc3431eb7e130ff
Rubendvb <rubenvasquez556@gmail.com>
2020-12-16 22:57:35 -0300

    tentando

--- pomodoro/front-end/src/app/task/task.component.ts
+++ pomodoro/front-end/src/app/task/task.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../tasks.service';
+import { Task } from '../models/Task'
 
 @Component({
   selector: 'app-task',
@@ -7,64 +8,34 @@ import { TasksService } from '../tasks.service';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-
-  minutos: number;
-  segundos: number;
-  vezes: number = 0;
   
-  tasks: any = [];
-  item: any = '';
-  item2: any = '';
+  tasks: Task[];
   
   ocultarEdit: boolean = true;
   
   constructor(private tasksService: TasksService) { }
   
+  
   ngOnInit(): void {
-    this.getTask()
-  }
-  getTask() {
-    this.tasksService.getTasks().subscribe(data => this.tasks= data)
-  }
-  postTask(){
-    this.tasksService.postTask
-  }
-  aggTarefa():void {
-    /* if (this.vezes % 4 === 0) {
-      this.minutos = 25
-      this.segundos = 0
-    } */
-    this.tasks.push(this.item)
-    this.item = '';
-  }
-  
-  myValue;
-  editTarefa(i): void {
-    this.ocultarEdit = false;
-    this.item2 = this.tasks[i];
-    this.myValue = i;
-  }
-  
-  actTarefa(): void {
-    this.ocultarEdit = true;
-    let i = this.myValue;
-    for ( let j = 0; j < this.tasks.length; j++) {
-      if (i == j) {
-        this.tasks[i] = this.item2;
-        this.item2 = '';
-        console.log(this.item2.value);
+    
+      this.tasksService.getTasks().subscribe((data: Task[]) => {
+        this.tasks = data
+      })
+    }
+    
+    deleteTask(id: any) {
+      var resposta = confirm('Deseja eliminar tarefa?');
+      if (resposta) {
+        this.tasks.splice(id, 1)
       }
-    }
-  }
+      console.log(id)    
+     } 
   
-  deletTarefa(i) {
-    var resposta = confirm('Deseja eliminar tarefa?');
-    if (resposta) {
-      this.tasks.splice( i, 1)
+     createdNewTask(task) {
+     this.tasksService.createdNewTask(task)
     }
-  }
-  
 
-
-
+    editTask() {
+      console.log('editando')
+    }
 }
--- pomodoro/front-end/src/app/tasks.service.ts
+++ pomodoro/front-end/src/app/tasks.service.ts
@@ -6,15 +6,43 @@ import { HttpClient } from '@angular/common/http'
 })
 export class TasksService {
 
+  url = 'http://localhost:3000/api/tasks';
 
   constructor(private http: HttpClient) { }
-
+  
   getTasks() {
-    return this.http.get('http://localhost:3000/api/tasks')
+    return this.http.get(`${this.url}`)
   }
 
-  postTask() {
-    
+  editTask(task_id) {
+    console.log(task_id)
+    return this.http.get(`${this.url}/${task_id}`)
   }
+
+  updateTask(task: any, task_id) {
+   console.log(task_id)
+    const tasks = {
+      task,
+      task_id,
+    }
+    console.log(tasks)
+    return this.http.put(`${this.url}/${task_id}`, tasks).subscribe((res) => console.log('aaaaaaaaaaaaaaaa'))
+   } 
+  
+  deleteTask(id) {
+    console.log('serviço')
+    return this.http.delete(`${this.url}/${id}`)
+  }
+  createdNewTask(task) {
+    const tarefa = {  
+      task 
+    }
+    console.log(task)
+    
+    return this.http.post(this.url, tarefa).subscribe((res) =>
+    console.log('aquiiii')
+    )
+  }
+
 
 }
